6622d3dd86069cc4987801779030a14e5ff8ce15
ohexus <moskalenko.taras.d@gmail.com>
2020-09-02 15:57:08 +0300

    added ability to rank up unit

--- routes/unit.routes.ts
+++ routes/unit.routes.ts
@@ -2,8 +2,11 @@ import { Response, Router } from 'express';
 import ExtendedRequest from '../interfaces/requests/ExtendedRequest';
 import {
   postCreateUnit,
+  postRankUpUnit,
+  getUnitById,
   getAllUnitsByUser,
   getAllUnits,
+  postClearUnitsDB,
 } from '../controllers/unit.controller';
 
 const router = Router();
@@ -11,12 +14,22 @@ const router = Router();
 router.post('/create', (req: ExtendedRequest, res: Response) =>
   postCreateUnit(req, res),
 );
+router.post('/rankUp', (req: ExtendedRequest, res: Response) =>
+  postRankUpUnit(req, res),
+);
 
 router.get('/all', (req: ExtendedRequest, res: Response) =>
   getAllUnitsByUser(req, res),
 );
 router.get('/all-dev', (req: ExtendedRequest, res: Response) =>
   getAllUnits(req, res),
+);
+router.get('/:id', (req: ExtendedRequest, res: Response) =>
+  getUnitById(req, res),
+);
+
+router.post('/clearUnitsDB', (req: ExtendedRequest, res: Response) =>
+  postClearUnitsDB(req, res),
 );
 
 const unitRouter = router;
