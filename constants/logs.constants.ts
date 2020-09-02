ea70e63d13a330ba51cf216c3efea4c42ac91f9a
ohexus <moskalenko.taras.d@gmail.com>
2020-09-02 11:25:18 +0300

    added new statuses to logs.constants.ts

--- constants/logs.constants.ts
+++ constants/logs.constants.ts
@@ -10,11 +10,12 @@ const LOGS = {
     AVATAR_UPLOAD: 'Avatar was successfully uploaded',
     USER_INFO_UPDATE: 'User was updated successfully',
     UNIT_CREATE: 'Unit created successfully',
+    LOOTING_CREATE: 'Looting created successfully',
   },
   ERROR: {
     DEFAULT: 'Request is failed!',
-    LOGIN: 'Login failed. Can not find such user!',
-    USER_SAVE: 'Can not add user to DB!',
+    LOGIN: "Login failed. Can't find such user!",
+    USER_SAVE: "Can't add user to DB!",
     REGISTER: 'Registration was failed!',
     JWT_EXPIRED: 'JWT token is expired!',
     PASS_COMPARING: 'Password do not match',
@@ -22,8 +23,10 @@ const LOGS = {
     USER_NOT_EXIST: 'User is not exist!',
     AVATAR_UPLOAD: 'Avatar upload is failed!',
     UNAUTHORIZED: 'Unauthorized',
-    UNIT_CREATE: 'Can not create unit',
-    GET_UNITS: 'Can not get units',
+    UNIT_CREATE: "Can't create unit",
+    LOOTING_CREATE: "Can't create looting",
+    GET_UNITS: "Can't get units",
+    GET_LOOTINGS: "Can't get looting",
   },
 };
 
