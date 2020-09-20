1ddebd549a6e9551a4297fa508d630fb3ae34acc
ohexus <moskalenko.taras.d@gmail.com>
2020-09-20 19:26:34 +0300

    fixed XP promotion values

--- constants/units/XP_PROMOTION.ts
+++ constants/units/XP_PROMOTION.ts
@@ -3,11 +3,11 @@ import { UnitPromotion } from '../../interfaces/units/UnitPromotion';
 const XP_PROMOTION = {
   BARQUE: 1000,
   SLOOP: 2250,
-  SCHOONER: 2750,
-  CARAVEL: 4500,
-  BRIG: 6500,
-  FRIGATE: 9000,
-  BATTLESHIP: 12000,
+  SCHOONER: 3750,
+  CARAVEL: 5500,
+  BRIG: 7500,
+  FRIGATE: 10000,
+  BATTLESHIP: 13000,
 } as UnitPromotion;
 
 export default XP_PROMOTION;
