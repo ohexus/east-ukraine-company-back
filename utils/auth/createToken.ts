77f006c0a32948eed1906477bc8941fd962fedd7
ohexus <moskalenko.taras.d@gmail.com>
2020-10-07 04:52:59 +0300

    token expires in 2 days now

--- utils/auth/createToken.ts
+++ utils/auth/createToken.ts
@@ -5,6 +5,6 @@ const secret: string = process.env.JWT_SECRET || config.get('JWT_SECRET');
 
 export default function createToken(userId: string): string {
   return jwt.sign({ userId }, secret, {
-    expiresIn: '1d',
+    expiresIn: '2 days',
   });
 }
